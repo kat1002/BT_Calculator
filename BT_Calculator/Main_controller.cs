3f68233c451eaec5002bd3f9b939f19fcdecc4bd
taola <taola@KAT>
2022-11-17 09:47:58 +0700

    Add project files.

---
+++ BT_Calculator/Main_controller.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BT_Calculator
+{
+    public class Main_controller
+    {
+        #region Method
+
+        public long Addition(string[] data)
+        {
+            long res = 0;
+            foreach (var item in data)
+            {
+                var i_item = Convert.ToInt64(item);
+                res += i_item;
+            }
+
+            return res;
+        }
+
+        public long Subtraction(string[] data)
+        {
+            long res = int.Parse(data[0]);
+            for (int index = 1; index < data.Length; ++index)
+            {
+                var i_item = Convert.ToInt64(data[index]);
+                res -= i_item;
+            }
+
+            return res;
+        }
+
+        public long Multiplication(string[] data)
+        {
+            long res = 1;
+            foreach (var item in data)
+            {
+                var i_item = Convert.ToInt64(item);
+                res *= i_item;
+            }
+
+            return res;
+        }
+
+        public void Division(string[] data)
+        {
+            double res = Convert.ToDouble(data[0]);
+            double temp = Convert.ToDouble(data[1]);
+
+            if (temp == 0)
+            {
+
+                Console.WriteLine("Cant divide by 0");
+                return;
+            }
+
+            Console.WriteLine(res / temp);
+        }
+
+        public void Print(double text)
+        {
+            Console.WriteLine("Result: " + text);
+        }
+
+        public bool CheckOpID(string opid)
+        {
+            if (opid == null) return false;
+            for (int index = 0; index < opid.Length; ++index)
+            {
+                if (opid[index] < '0' || opid[index] > '9') return false;
+            }
+            return true;
+        }
+
+        public void Input(ref int option)
+        {
+            StartPoint:
+            try
+            {
+                string optionID = Console.ReadLine();
+                optionID = optionID.Trim();
+                
+                while (true)
+                {
+                    while (!CheckOpID(optionID))
+                    {
+                        Console.Write("Wrong input! Please choose again: ");
+                        optionID = Console.ReadLine();
+                        optionID = optionID.Trim();
+                    }
+
+                    option = Convert.ToInt32(optionID);
+                    if (option >= 1 && option <= 5) break;
+                }
+            }
+            catch (Exception e)
+            {
+                goto StartPoint;
+            }
+        } 
+
+        #endregion
+
+
+    }
+}
---
+++ BT_Calculator/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BT_Calculator
+{
+    internal class Program
+    {
+      
+        static void Main(string[] args)
+        {
+            Main_controller _cls = new Main_controller();
+
+        bool running = true;
+            while (running) {
+                int option = 5;
+                Console.WriteLine("This is ur calculator" +
+                    "\n1.+" +
+                    "\n2.- " +
+                    "\n3.*" +
+                    "\n4./" +
+                    "\n5.Exit\n");
+                Console.Write("Choose your option: ");
+                //Input
+                _cls.Input(ref option);
+
+                if(option == 5) System.Environment.Exit(0);
+
+                //Body
+                StartBody:
+                Console.Write("Enter 2 values: ");
+
+                var input = Console.ReadLine();
+                input = input.Trim();
+                var data = input.Split(' ');
+                double d_result;
+
+                try
+                {
+                    switch (option)
+                    {
+                        case 1:
+                            d_result = _cls.Addition(data);
+                            _cls.Print(d_result);
+                            break;
+
+                        case 2:
+                            d_result = _cls.Subtraction(data);
+                            _cls.Print(d_result);
+                            break;
+                       
+                        case 3:
+                            d_result = _cls.Multiplication(data);
+                            _cls.Print(d_result);
+                            break;
+                        
+                        case 4:
+                            _cls.Division(data);
+                            break;
+                    }
+
+
+                }
+                catch(Exception e)
+                {
+                    goto StartBody;
+                }
+                //Ask if user want to end the program
+                EndPoint:
+                Console.Write("Do you want to end the program? Y/N: ");
+                var op2 = Console.ReadLine();
+                op2 = op2.Trim();
+                if (op2 == "Y" || op2 == "y") System.Environment.Exit(0);
+            }
+        }
+        
+    }
+}
